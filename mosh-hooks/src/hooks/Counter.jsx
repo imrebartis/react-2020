195b4ba9239f80cd8933be11a72b086d6bd758d6
Imre Bartis <imre.bartis@siili.com>
2020-02-19 16:38:52 +0200

    custom hooks

--- mosh-hooks/src/hooks/Counter.jsx
+++ mosh-hooks/src/hooks/Counter.jsx
@@ -1,25 +1,21 @@
-import React, { useState, Fragment, useEffect } from "react";
+import React, { useState, Fragment } from "react";
+import useDocumentTitle from "./useDocumentTitle";
 
 function Counter(props) {
   // const array = useState(0);
   // const count = array[0]; // this.state.count;
   // const setState = array[1]; // this.setState();
   const [count, setCount] = useState(0);
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
-  useEffect(() => {
-    document.title = ` ${name} has clicked ${count} times`;
-
-    // componentWillUnmount
-    return () => {
-      console.log('cleanup')
-    }
-  });
+  useDocumentTitle(`${name} has clicked ${count} times`);
 
   return (
     <Fragment>
       <input type="text" onChange={e => setName(e.target.value)} />
-      <div>{name} has clicked {count} times</div>
+      <div>
+        {name} has clicked {count} times
+      </div>
       <button onClick={() => setCount(count + 1)}>Increase </button>
     </Fragment>
   );

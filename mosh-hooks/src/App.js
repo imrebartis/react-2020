0c8ff0d2094145b2789b9d211e971c0c1b954fba
Imre Bartis <imre.bartis@siili.com>
2020-02-20 16:16:54 +0200

    fetch data with hooks

--- mosh-hooks/src/App.js
+++ mosh-hooks/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import Counter from './hooks/Counter';
+import Users from './hooks/Users';
 
 function App() {
-  return <Counter />;
+  return <Users />;
 }
 
 export default App;

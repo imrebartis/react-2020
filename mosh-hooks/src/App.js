a57748b05290cbeda2b57801d44c0cdcb9464106
Imre Bartis <imre.bartis@siili.com>
2020-02-23 10:57:48 +0200

    update the context

--- mosh-hooks/src/App.js
+++ mosh-hooks/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import MoviePage from "./context/MoviePage";
-import UserContext from './context/userContext';
+import UserContext from "./context/userContext";
+import Login from "./context/Login";
 
 export default class App extends Component {
-  state = { currentUser: {name: 'Howy'} }
+  handleLoggedIn = username => {
+    console.log("Getting the user: " + username);
+    const user = { name: "Howy" };
+    this.setState({ currentUser: user });
+  };
+
+  state = { currentUser: null };
   render() {
     return (
-      <UserContext.Provider value={this.state.currentUser}>
+      <UserContext.Provider
+        value={{
+          currentUser: this.state.currentUser,
+          onLoggedIn: this.handleLoggedIn
+        }}
+      >
         <div>
           <MoviePage />
+          <Login />
         </div>
       </UserContext.Provider>
     );

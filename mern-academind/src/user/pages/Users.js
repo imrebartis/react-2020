733d17ea0c93d111ba181c94c910c7b096dbca1b
Imre Bartis <imre.bartis@siili.com>
2020-03-17 15:22:28 +0200

    add a UsersList component

--- mern-academind/src/user/pages/Users.js
+++ mern-academind/src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users works</h2>;
-}
- 
+  const USERS = [
+    {
+      id: "u1",
+      name: "Frank",
+      image: "https://picsum.photos/200/300",
+      places: 2
+    }
+  ];
+  return <UsersList items={USERS} />;
+};
+
 export default Users;

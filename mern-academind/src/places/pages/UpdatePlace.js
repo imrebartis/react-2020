572be3b1aebb5ef5c848a024e8bf04f9da35d3ac
Imre Bartis <imre.bartis@siili.com>
2020-04-29 15:21:49 +0300

    adjust the form hook

--- mern-academind/src/places/pages/UpdatePlace.js
+++ mern-academind/src/places/pages/UpdatePlace.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
-import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
-
 
 import './PlaceForm.css';
 
@@ -39,30 +41,58 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const placeId = useParams().placeId;
-
   const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
-  const [formState, inputHandler] = useForm({
-    title: {
-      value: identifiedPlace.title,
-      isValid: true
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      description: {
+        value: '',
+        isValid: false,
+      },
     },
-    description: {
-      value: identifiedPlace.description,
-      isValid: true
-    }
-  }, true)
+    false,
+  );
 
-  const placeUpdateSubmitHandler = event => {
+  useEffect(() => {
+    setFormData(
+      {
+        title: {
+          value: identifiedPlace.title,
+          isValid: true,
+        },
+        description: {
+          value: identifiedPlace.description,
+          isValid: true,
+        },
+      },
+      true,
+    );
+    setIsLoading(false);
+  }, [setFormData, identifiedPlace]);
+
+  const placeUpdateSubmitHandler = (event) => {
     event.preventDefault();
     console.log(formState.inputs);
-  }
+  };
 
   if (!identifiedPlace) {
     return (
       <div className="center">
         <h2>Could not find place.</h2>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
       </div>
     );
   }
@@ -90,7 +120,9 @@ const UpdatePlace = () => {
         initialValue={formState.inputs.description.value}
         initialValid={formState.inputs.description.isValid}
       />
-      <Button type="submit" disabled={!formState.isValid}>Update place</Button>
+      <Button type="submit" disabled={!formState.isValid}>
+        Update place
+      </Button>
     </form>
   );
 };

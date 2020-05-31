16cfc3995e604b9047d440b1075c117dcabc214c
Imre Bartis <imre.bartis@siili.com>
2020-05-31 14:49:37 +0300

    fetch data on the server with getInitialProps

--- mern-rb/pages/index.js
+++ mern-rb/pages/index.js
@@ -1,5 +1,27 @@
-function Home() {
+import React from "react";
+import axios from "axios";
+
+function Home({ products }) {
+  console.log(products);
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, []);
+
+  // async function getProducts() {
+  //   const url = "http://localhost:3000/api/products";
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  // }
+
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  return { products: response.data };
+  // return response data as an object
+  // this object will be merged with existing props
+};
 
 export default Home;

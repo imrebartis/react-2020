52901e2835ed516441a547ab490c787beb857ac5
Imre Bartis <imre.bartis@siili.com>
2020-06-04 15:14:58 +0300

    create product form

--- mern-rb/pages/api/products.js
+++ mern-rb/pages/api/products.js
@@ -1,5 +1,5 @@
 // import products from "../../static/products.json";
-import Product from '../../models/Product';
+import Product from "../../models/Product";
 import connectDb from "../../utils/connectDb";
 
 connectDb();

d14afb5603c090a80f2d5fbb8c6b627799b66544
Imre Bartis <imre.bartis@siili.com>
2020-06-14 14:18:24 +0300

    get current user from token, protect auth routes

--- mern-rb/components/_App/Header.js
+++ mern-rb/components/_App/Header.js
@@ -7,9 +7,9 @@ Router.onRouteChangeStart = () => NProgress.start();
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.done();
 
-function Header() {
+function Header({ user }) {
   const router = useRouter();
-  const user = false;
+  console.log(user)
 
   function isActive(route) {
     return route === router.pathname;

323726bc7017abda9d8909bd71cb8d333812e3d6
Imre Bartis <imre.bartis@siili.com>
2020-07-13 05:10:43 +0300

    toggle state with useState

--- react-hooks-intro/src/App.js
+++ react-hooks-intro/src/App.js
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
 
   const incrementCount = () => {
-    setCount(prevCount => prevCount + 1)
-  }
+    setCount((prevCount) => prevCount + 1);
+  };
 
-  return <button onClick={incrementCount}>hola! I was clicked {count} times</button>;
-}
+  const toggleLight = () => {
+    setIsOn((prevIsOn) => !prevIsOn);
+  };
+
+  return (
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}>
+        hola! I was clicked {count} times
+      </button>
+
+      <h2>Toggle Light</h2>
+      <img
+        src={
+          isOn ? 'https://icon.now.sh/highlight/fd0'
+          : 'https://icon.now.sh/highlight/aaa'
+        }
+        style={{
+          height: '50px',
+          width: '50px',
+          background: isOn ? 'yellow' : 'grey',
+        }}
+        onClick={toggleLight}
+        alt="flashlight"
+      />
+    </>
+  );
+};
 
 export default App;

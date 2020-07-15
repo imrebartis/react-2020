ad385d33379abb8017a5f067e35377825744a963
Imre Bartis <imre.bartis@siili.com>
2020-07-15 08:46:45 +0300

    clean up side effects in useEffect

--- react-hooks-intro/src/App.js
+++ react-hooks-intro/src/App.js
@@ -3,14 +3,27 @@ import React, { useState, useEffect } from 'react';
 const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
 
   const incrementCount = () => {
     setCount((prevCount) => prevCount + 1);
   };
 
   useEffect(() => {
-    document.title = `You've clicked ${count} times`
-  })
+    document.title = `You've clicked ${count} times`;
+    window.addEventListener('mouseover', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mouseover', handleMouseMove);
+    };
+  }, [count]);
+
+  const handleMouseMove = (event) => {
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY,
+    });
+  };
 
   const toggleLight = () => {
     setIsOn((prevIsOn) => !prevIsOn);
@@ -26,8 +39,9 @@ const App = () => {
       <h2>Toggle Light</h2>
       <img
         src={
-          isOn ? 'https://icon.now.sh/highlight/fd0'
-          : 'https://icon.now.sh/highlight/aaa'
+          isOn
+            ? 'https://icon.now.sh/highlight/fd0'
+            : 'https://icon.now.sh/highlight/aaa'
         }
         style={{
           height: '50px',
@@ -37,6 +51,9 @@ const App = () => {
         onClick={toggleLight}
         alt="flashlight"
       />
+      <h2>Mouse position</h2>
+      {JSON.stringify(mousePosition, null, 2)}
+      <br />
     </>
   );
 };

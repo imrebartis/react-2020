d7e61f867a91d4fc97ed9daa40cb92c931ee0810
Imre Bartis <imre.bartis@siili.com>
2020-07-21 13:55:03 +0300

    fetch data on component mount with useEffect

--- react-hooks-intro/src/App.js
+++ react-hooks-intro/src/App.js
@@ -1,106 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const initialLocationState = {
-  latitude: null,
-  longitude: null,
-  speed: null,
-};
-
-const App = () => {
-  const [count, setCount] = useState(0);
-  const [isOn, setIsOn] = useState(false);
-  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
-  const [status, setStatus] = useState(navigator.onLine);
-  const [{latitude, longitude, speed}, setLocation] = useState(initialLocationState);
-  let mounted = true;
-
-  const incrementCount = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+export default function App() {
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    document.title = `You've clicked ${count} times`;
-    window.addEventListener('mouseover', handleMouseMove);
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
-    navigator.geolocation.getCurrentPosition(handleGeolocation);
-    const watchId = navigator.geolocation.watchPosition(handleGeolocation);
-
-    return () => {
-      window.removeEventListener('mouseover', handleMouseMove);
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-      mounted = false;
-      navigator.geolocation.clearWatch(watchId);
-    };
-  }, [count]);
-
-  const handleGeolocation = (event) => {
-    if (mounted) {
-      setLocation({
-        latitude: event.coords.latitude,
-        longitude: event.coords.longitude,
-        speed: event.coords.speed,
+    axios
+      .get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then((response) => {
+        console.log(response.data);
+        setResults(response.data.hits);
       });
-    }
-  };
-
-  const handleOnline = () => {
-    setStatus(true);
-  };
-
-  const handleOffline = () => {
-    setStatus(false);
-  };
-
-  const handleMouseMove = (event) => {
-    setMousePosition({
-      x: event.pageX,
-      y: event.pageY,
-    });
-  };
-
-  const toggleLight = () => {
-    setIsOn((prevIsOn) => !prevIsOn);
-  };
+  }, []);
 
   return (
     <>
-      <h2>Counter</h2>
-      <button onClick={incrementCount}>
-        hola! I was clicked {count} times
-      </button>
-
-      <h2>Toggle Light</h2>
-      <img
-        src={
-          isOn
-            ? 'https://icon.now.sh/highlight/fd0'
-            : 'https://icon.now.sh/highlight/aaa'
-        }
-        style={{
-          height: '50px',
-          width: '50px',
-          background: isOn ? 'yellow' : 'grey',
-        }}
-        onClick={toggleLight}
-        alt="flashlight"
-      />
-      <h2>Mouse position</h2>
-      {JSON.stringify(mousePosition, null, 2)}
-      <br />
-
-      <h2>Network Status</h2>
-      <p>
-        You are <strong>{status ? 'online' : 'offline'}</strong>
-      </p>
-
-      <h2>Geolocation</h2>
-      <p>Latitude: {latitude}</p>
-      <p>Longitude: {longitude}</p>
-      <p>Speed: {speed ? speed : '0'}</p>
+      <ul>
+        {results.map((result) => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
     </>
   );
-};
-
-export default App;
+}
--- react-hooks-intro/src/index.js
+++ react-hooks-intro/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Register';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(

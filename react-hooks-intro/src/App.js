ff8f38a93d1a9c9307b27d4e290998b1d6ea1620
Imre Bartis <imre.bartis@siili.com>
2020-07-23 05:45:39 +0300

    fetch search results with useEffect

--- react-hooks-intro/src/App.js
+++ react-hooks-intro/src/App.js
@@ -3,26 +3,35 @@ import axios from 'axios';
 
 export default function App() {
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('reacthooks');
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [query]);
 
   const fetchData = async () => {
     const response = await axios.get(
-      'http://hn.algolia.com/api/v1/search?query=reacthooks',
+      `http://hn.algolia.com/api/v1/search?query=${query}`,
     );
+    console.log(response.data.hits);
     setResults(response.data.hits);
   };
 
   return (
     <>
+      <input type="text" onChange={(event) => setQuery(event.target.value)} />
       <ul>
-        {results.map((result) => (
-          <li key={result.objectID}>
-            <a href={result.url}>{result.title}</a>
-          </li>
-        ))}
+        {results.map((result) => {
+          return result.title ? (
+            <li key={result.objectID}>
+              <a href={result.url}>{result.title}</a>
+            </li>
+          ) : (
+            <li key={result.objectID}>
+              <a href={result.url}>{result.story_title}</a>
+            </li>
+          );
+        })}
       </ul>
     </>
   );

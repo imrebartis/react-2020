e0ac010fe60d9d35802c5b371b34304ae0d43c63
Imre Bartis <imre.bartis@siili.com>
2020-07-24 05:04:42 +0300

    fetch data upon submitting form

--- react-hooks-intro/src/App.js
+++ react-hooks-intro/src/App.js
@@ -2,36 +2,55 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function App() {
+  const [submitting, setSubmitting] = useState(true);
   const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('reacthooks');
+  const [query, setQuery] = useState('react hooks');
 
   useEffect(() => {
-    fetchData();
-  }, [query]);
+    const fetchData = async () => {
+      const response = await axios.get(
+        `http://hn.algolia.com/api/v1/search?query=${query}`,
+      );
+      console.log(response.data.hits);
+      setResults(response.data.hits);
+      setSubmitting(false);
+    };
 
-  const fetchData = async () => {
-    const response = await axios.get(
-      `http://hn.algolia.com/api/v1/search?query=${query}`,
-    );
-    console.log(response.data.hits);
-    setResults(response.data.hits);
-  };
+    if (submitting) {
+      fetchData();
+    }
+  }, [submitting, query]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+  }
 
   return (
     <>
-      <input type="text" onChange={(event) => setQuery(event.target.value)} />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={(event) => setQuery(event.target.value)}
+        value={query}
+      />
+      <button type="submit">
+        Search
+      </button>
+    </form>
       <ul>
-        {results.map((result) => {
-          return result.title ? (
-            <li key={result.objectID}>
-              <a href={result.url}>{result.title}</a>
-            </li>
-          ) : (
-            <li key={result.objectID}>
-              <a href={result.url}>{result.story_title}</a>
-            </li>
-          );
-        })}
+        {results &&
+          results.map((result) => {
+            return result.title ? (
+              <li key={result.objectID}>
+                <a href={result.url}>{result.title}</a>
+              </li>
+            ) : (
+              <li key={result.objectID}>
+                <a href={result.url}>{result.story_title}</a>
+              </li>
+            );
+          })}
       </ul>
     </>
   );

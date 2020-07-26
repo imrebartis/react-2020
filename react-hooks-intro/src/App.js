6c8e62d31dfe6225f48cdd9c6e808c479802729f
Imre Bartis <imre.bartis@siili.com>
2020-07-26 05:02:35 +0300

    display loading state

--- react-hooks-intro/src/App.js
+++ react-hooks-intro/src/App.js
@@ -5,15 +5,18 @@ export default function App() {
   const [submitting, setSubmitting] = useState(true);
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState('react hooks');
+  const [loading, setLoading] = useState(false);
   const searchInputRef = useRef();
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const response = await axios.get(
         `http://hn.algolia.com/api/v1/search?query=${query}`,
       );
       console.log(response.data.hits);
       setResults(response.data.hits);
+      setLoading(false);
       setSubmitting(false);
     };
 
@@ -22,46 +25,48 @@ export default function App() {
     }
   }, [submitting, query]);
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     setSubmitting(true);
-  }
+  };
 
   const handleClearSearch = () => {
     setQuery('');
     searchInputRef.current.focus();
-  }
+  };
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        onChange={(event) => setQuery(event.target.value)}
-        value={query}
-        ref={searchInputRef}
-      />
-      <button type="submit">
-        Search
-      </button>
-      <button type="button" onClick={handleClearSearch}>
-        Clear
-      </button>
-    </form>
-      <ul>
-        {results &&
-          results.map((result) => {
-            return result.title ? (
-              <li key={result.objectID}>
-                <a href={result.url}>{result.title}</a>
-              </li>
-            ) : (
-              <li key={result.objectID}>
-                <a href={result.url}>{result.story_title}</a>
-              </li>
-            );
-          })}
-      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(event) => setQuery(event.target.value)}
+          value={query}
+          ref={searchInputRef}
+        />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>
+          Clear
+        </button>
+      </form>
+      {loading ? (
+        <div>Loading results...</div>
+      ) : (
+        <ul>
+          {results &&
+            results.map((result) => {
+              return result.title ? (
+                <li key={result.objectID}>
+                  <a href={result.url}>{result.title}</a>
+                </li>
+              ) : (
+                <li key={result.objectID}>
+                  <a href={result.url}>{result.story_title}</a>
+                </li>
+              );
+            })}
+        </ul>
+      )}
     </>
   );
 }

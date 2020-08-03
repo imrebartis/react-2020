acdf1e3f326bf60ec9ba83a990090f924e2090b5
Imre Bartis <imre.bartis@siili.com>
2020-08-03 03:29:29 +0300

    toggle

--- react-hooks-intro/src/components/TodoList.js
+++ react-hooks-intro/src/components/TodoList.js
@@ -1,21 +1,36 @@
 import React, { useContext } from 'react';
 import TodosContext from '../context';
 
-import { Icon } from 'react-icons-kit'
-import {pen_1} from 'react-icons-kit/ikons/pen_1'
-import {bin} from 'react-icons-kit/ikons/bin'
+import { Icon } from 'react-icons-kit';
+import { pen_1 } from 'react-icons-kit/ikons/pen_1';
+import { bin } from 'react-icons-kit/ikons/bin';
 
 export default function TodoList() {
-  const { state } = useContext(TodosContext);
-  const title = state.todos.length > 0 ? `${state.todos.length} Todos` : `No todos`;
+  const { state, dispatch } = useContext(TodosContext);
+  const title =
+    state.todos.length > 0 ? `${state.todos.length} Todos` : `No todos`;
 
   return (
     <div className="container mx-auto max-w-md text-center font-mono">
-    <h1 className="font-bold">{title}</h1>
+      <h1 className="font-bold">{title}</h1>
       <ul className="list-reset text-white p-0">
         {state.todos.map((todo) => (
-          <li className="flex items-center bg-orange-600 border-black border-dashed border-2 my-2 py-4" key={todo.id}>
-            <span className="flex-1 ml-12 cursor-pointer">{todo.text}</span>
+          <li
+            className="flex items-center bg-orange-600 border-black border-dashed border-2 my-2 py-4"
+            key={todo.id}
+          >
+            <span
+              onDoubleClick={() =>
+                dispatch({ type: 'TOGGLE_TODO', payload: todo })
+              }
+              className={`${
+                todo.completed
+                  ? 'flex-1 ml-12 cursor-pointer line-through text-gray-800'
+                  : 'flex-1 ml-12 cursor-pointer'
+              }`}
+            >
+              {todo.text}
+            </span>
             <button className="mr-2">
               <Icon className="text-blue-800" icon={pen_1} />
             </button>
--- react-hooks-intro/src/reducer.js
+++ react-hooks-intro/src/reducer.js
@@ -1,5 +1,15 @@
 export default function reducer(state, action) {
-  switch(action.type) {
+  switch (action.type) {
+    case 'TOGGLE_TODO':
+      const toggleTodos = state.todos.map((t) =>
+        t.id === action.payload.id
+          ? { ...action.payload, completed: !action.payload.completed }
+          : t,
+      );
+      return {
+        ...state,
+        todos: toggleTodos,
+      };
     default:
       return state;
   }

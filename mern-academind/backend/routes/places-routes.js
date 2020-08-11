c65c460bbf96019a1be135b7315d46e03e778e70
Imre Bartis <imre.bartis@siili.com>
2020-08-11 04:08:39 +0300

    get a place by user id

--- mern-academind/backend/routes/places-routes.js
+++ mern-academind/backend/routes/places-routes.js
@@ -9,19 +9,27 @@ const DUMMY_PLACES = [
     description: 'One of the most famous sky scrapers in the world!',
     location: {
       lat: 40.7484474,
-      lng: -73.9871516
+      lng: -73.9871516,
     },
     address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  }
+    creator: 'u1',
+  },
 ];
 
 router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
-  const place = DUMMY_PLACES.find(p => {
+  const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
-  res.json({place}); // => { place } => { place: place }
+  res.json({ place }); // => { place } => { place: place }
+});
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  res.json({ place });
 });
 
 module.exports = router;

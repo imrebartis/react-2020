3c80712451edebd1ce9563956470ff69d8d81d98
Imre Bartis <imre.bartis@siili.com>
2020-08-17 02:43:26 +0300

    start work on login

---
+++ react-social-media/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Login from './components/Login';
+
+function App() {
+  const [user, setUser] = useState('');
+
+  if (!user) {
+    return <Login setUser={setUser} />;
+  }
+
+  return <div>app</div>;
+}
+
+export default App;

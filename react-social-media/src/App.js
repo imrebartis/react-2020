d517a0523dd6f4cbac0adead1a303007e5108b01
Imre Bartis <imre.bartis@siili.com>
2020-08-18 07:59:13 +0300

    set up Header; show document title; set up CreatePost

--- react-social-media/src/App.js
+++ react-social-media/src/App.js
@@ -1,14 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import Login from './components/Login';
+import Header from './components/Header';
+import CreatePost from './components/CreatePost';
 
 function App() {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState('ddf');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    document.title = user ? `${user}'s Feed` : 'Please log in';
+  }, [user]);
 
   if (!user) {
     return <Login setUser={setUser} />;
   }
 
-  return <div>app</div>;
+  return (
+    <>
+      <Header user={user} setUser={setUser} />
+      <CreatePost user={user} setPosts={setPosts} posts={posts} />
+      {posts.map((post, i) => (
+        <Fragment key={i}>
+          {post.image && (
+            <img
+              style={{ height: 100, width: 200, objectFit: 'cover' }}
+              src={URL.createObjectURL(post.image)}
+              alt="Post cover"
+            />
+          )}
+          <p>{post.content}</p>
+          <div>{user}</div>
+        </Fragment>
+      ))}
+    </>
+  );
 }
 
 export default App;
--- react-social-media/src/components/CreatePost.js
+++ react-social-media/src/components/CreatePost.js
@@ -1,8 +1,42 @@
+import React, { useState, useRef } from 'react';
 
-import React from 'react';
+function CreatePost({ user, setPosts, posts }) {
+  const [content, setContent] = useState('');
+  const [image, setImage] = useState(null);
+  const imageInputRef = useRef();
 
-function CreatePost() {
-  return <div>create post</div>;
+  function handleSubmit(event) {
+    event.preventDefault();
+    const post = {
+      content,
+      image,
+      user,
+    };
+    const newPosts = [post, ...posts];
+    setPosts(newPosts);
+    setContent('');
+    imageInputRef.current.value = '';
+  }
+
+  return (
+    <div>
+      <h1>Create New Post</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(event) => setContent(event.target.value)}
+          placeholder="Add post content"
+          value={content}
+        />
+        <input
+          type="file"
+          onChange={(event) => setImage(event.target.files[0])}
+          ref={imageInputRef}
+        />
+        <button type="submit">Submit Post</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePost;

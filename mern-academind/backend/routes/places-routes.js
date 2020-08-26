14dced280ce629feb292d24af5599588c4a55ec9
Imre Bartis <imre.bartis@siili.com>
2020-08-26 16:11:47 +0300

    add patch routes to update places

--- mern-academind/backend/routes/places-routes.js
+++ mern-academind/backend/routes/places-routes.js
@@ -10,4 +10,7 @@ router.get('/user/:uid', PlacesControllers.getPlaceByUserId);
 
 router.post('/', PlacesControllers.createPlace);
 
+router.patch('/:pid', PlacesControllers.updatePlace);
+router.delete('/:pid', PlacesControllers.deletePlace);
+
 module.exports = router;

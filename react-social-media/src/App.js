9923fcd42add87af75a87fea5206490de2b5364f
Imre Bartis <imre.bartis@siili.com>
2020-08-31 10:25:59 +0300

    display error message; start styling app.

--- react-social-media/src/App.js
+++ react-social-media/src/App.js
@@ -13,17 +13,41 @@ const GET_TODOS = gql`
 `;
 
 function App() {
-  const { data, loading } = useQuery(GET_TODOS);
+  const { data, loading, error } = useQuery(GET_TODOS);
 
-  if (loading) return <div>loading...</div>;
+  if (loading) return <div>Loading todos...</div>;
+  if (error) return <div>Error fetching todos!</div>;
 
   return (
-    <div>
-      {data.todos.map((todo) => (
-        <p key={todo.id}>
-          <span>{todo.text}</span> <button>&times;</button>
-        </p>
-      ))}
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
+      <h1 className="f2-l">
+        GraphQL Checklist{' '}
+        <span role="img" aria-label="Checkmark">
+          ✅
+        </span>
+      </h1>
+      {/* Todo Form */}
+      <form className="mb3">
+        <input
+          className="pa2 f4 b--dashed"
+          type="text"
+          placeholder="Write your todo"
+        />
+        <button className="pa2 f4 bg-green" type="submit">
+          Create
+        </button>
+      </form>
+      {/* Todo List */}
+      <div className="flex items-center justify-center flex-column">
+        {data.todos.map((todo) => (
+          <p key={todo.id}>
+            <span>{todo.text}</span>{' '}
+            <button className="bg-transparent bn f4">
+              <span className="red">&times;</span>
+            </button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }

f5619475f62b769b811bf3b68121eeda6756d2c3
Imre Bartis <imre.bartis@siili.com>
2020-08-31 08:58:43 +0300

    list todos in the DOM.

--- react-social-media/src/App.js
+++ react-social-media/src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+`;
 
 function App() {
-  return <div>app</div>;
+  const { data, loading } = useQuery(GET_TODOS);
+
+  if (loading) return <div>loading...</div>;
+
+  return (
+    <div>
+      {data.todos.map((todo) => (
+        <p key={todo.id}>
+          <span>{todo.text}</span> <button>&times;</button>
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default App;

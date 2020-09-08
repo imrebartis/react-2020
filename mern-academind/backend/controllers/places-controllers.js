332ffc4797dd6fa7b81582baf93a8bc6ce964337
Imre Bartis <imre.bartis@siili.com>
2020-09-08 16:17:02 +0300

    use mapbox api to convert address into coordinates.

--- mern-academind/backend/controllers/places-controllers.js
+++ mern-academind/backend/controllers/places-controllers.js
@@ -1,6 +1,8 @@
 const { v4: uuidv4 } = require('uuid');
-const HttpError = require('../models/http-errors');
 const { validationResult } = require('express-validator');
+
+const HttpError = require('../models/http-errors');
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
   {
@@ -44,13 +46,20 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    return next(new HttpError('Invalid inputs passed, please check your data.', 422));
   }
 
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
 
   const createdPlace = {
     id: uuidv4(),
@@ -87,7 +96,7 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
-  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
     throw new HttpError('Could not find a place for that id.', 404);
   }
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
---
+++ mern-academind/backend/util/location.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const { MAPBOX_ACCESS_TOKEN } = require('../configuration.js');
+const HttpError = require('../models/http-errors');
+
+const getCoordsForAddress = async (address) => {
+  let data;
+  try {
+    const url = 'https://api.mapbox.com/geocoding/v5';
+    const endpoint = 'mapbox.places';
+    const searchText = encodeURIComponent(address);
+
+    const response = await axios({
+      method: 'GET',
+      url: `${url}/${endpoint}/${searchText}.json/?access_token=${MAPBOX_ACCESS_TOKEN}`,
+    });
+
+    data = response.data;
+  } catch (e) {
+    throw new HttpError('Something went wrong', 500);
+  }
+
+  if (!data || data.status === 'ZERO_RESULTS') {
+    throw new HttpError(
+      'Could not find location for the specified address.',
+      422,
+    );
+  }
+
+  const [lng, lat] = data.features[0].center;
+
+  return { lat, lng };
+};
+
+module.exports = getCoordsForAddress;

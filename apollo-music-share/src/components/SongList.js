071660326d06de439516da106c94c9b4d4fc2c52
Imre Bartis <imre.bartis@siili.com>
2020-09-12 19:13:24 +0300

    make (with the help of useEffect) SongList's song's pause/play button toggle when SongPlayer button's pause/play button is toggled.

--- apollo-music-share/src/components/SongList.js
+++ apollo-music-share/src/components/SongList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from 'react';
 import {
   CircularProgress,
   Card,
@@ -7,14 +7,16 @@ import {
   Typography,
   CardActions,
   IconButton,
-  makeStyles
-} from "@material-ui/core";
-import { PlayArrow, Save } from "@material-ui/icons";
-import { useSubscription } from "@apollo/react-hooks";
+  makeStyles,
+} from '@material-ui/core';
+import { PlayArrow, Pause, Save } from '@material-ui/icons';
+import { useSubscription } from '@apollo/react-hooks';
 import { GET_SONGS } from '../graphql/subscriptions';
+import { SongContext } from '../App';
 
 function SongList() {
-  const {data, loading, error }= useSubscription(GET_SONGS);
+  const { data, loading, error } = useSubscription(GET_SONGS);
+  const [currentSongPlaying, setCurrentSongPlaying] = useState(false);
 
   // const song = {
   //   title: "LÜNE",
@@ -26,17 +28,17 @@ function SongList() {
     return (
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          marginTop: 50
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: 50,
         }}
       >
         <CircularProgress />
       </div>
     );
   }
-  if (error) return <div>Error fetching songs</div>
+  if (error) return <div>Error fetching songs</div>;
 
   return (
     <div>
@@ -47,29 +49,37 @@ function SongList() {
   );
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   container: {
-    margin: theme.spacing(3)
+    margin: theme.spacing(3),
   },
   songInfoContainer: {
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center',
   },
   songInfo: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "space-between"
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
   },
   thumbnail: {
-    objectFit: "cover",
+    objectFit: 'cover',
     width: 140,
-    height: 140
-  }
+    height: 140,
+  },
 }));
 
 function Song({ song }) {
+  const { id } = song;
   const classes = useStyles();
+  const { state, dispatch } = useContext(SongContext);
   const { title, artist, thumbnail } = song;
+  const [currentSongPlaying, setCurrentSongPlaying] = useState(false);
+
+  useEffect(() => {
+    const isSongPlaying = state.isPlaying && id === state.song.id;
+    setCurrentSongPlaying(isSongPlaying);
+  }, [id, state.song.id, state.isPlaying]);
 
   return (
     <Card className={classes.container}>
@@ -86,7 +96,7 @@ function Song({ song }) {
           </CardContent>
           <CardActions>
             <IconButton size="small" color="primary">
-              <PlayArrow />
+              {currentSongPlaying ? <Pause /> : <PlayArrow />}
             </IconButton>
             <IconButton size="small" color="secondary">
               <Save />

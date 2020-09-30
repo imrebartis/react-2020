4caeb78f982db863bdce63f3335695eaaff9a391
Imre Bartis <imre.bartis@siili.com>
2020-09-30 20:26:21 +0300

    add user login

--- mern-academind/backend/controllers/users-controllers.js
+++ mern-academind/backend/controllers/users-controllers.js
@@ -21,62 +21,73 @@ const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
-      new HttpError('Invalid inputs passed, please check your data.', 422)
+      new HttpError('Invalid inputs passed, please check your data.', 422),
     );
   }
   const { name, email, password, places } = req.body;
 
   let existingUser;
-  
+
   try {
-    existingUser = await User.findOne({ email: email })
+    existingUser = await User.findOne({ email: email });
   } catch (err) {
     const error = new HttpError(
       'Signing up failed, please try again later.',
-      500
+      500,
     );
     return next(error);
   }
-  
+
   if (existingUser) {
     const error = new HttpError(
       'User exists already, please login instead.',
-      422
+      422,
     );
     return next(error);
   }
-  
+
   const createdUser = new User({
     name,
     email,
     image: 'https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg',
     password,
-    places
+    places,
   });
 
   try {
     await createdUser.save();
   } catch (err) {
+    const error = new HttpError('Signing up failed, please try again.', 500);
+    return next(error);
+  }
+
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
     const error = new HttpError(
-      'Signing up failed, please try again.',
-      500
+      'Logging in failed, please try again later.',
+      500,
     );
     return next(error);
   }
 
-  res.status(201).json({user: createdUser.toObject({ getters: true })});
-};
-
-const login = (req, res, next) => {
-  const { email, password } = req.body;
-
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('Could not identify user.', 401);
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      401,
+    );
+    return next(error);
   }
 
-  res.json({ message: 'logged in' });
+  res.json({ message: 'Logged in!' });
 };
 
 exports.getUser = getUser;

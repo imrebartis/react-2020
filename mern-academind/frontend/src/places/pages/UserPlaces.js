cad0f8c93bf5902c9d4a957233092595b058e041
Imre Bartis <imre.bartis@siili.com>
2020-11-05 17:57:25 +0200

    load places by user id.

--- mern-academind/frontend/src/places/pages/UserPlaces.js
+++ mern-academind/frontend/src/places/pages/UserPlaces.js
@@ -1,41 +1,40 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import PlaceList from "../components/PlaceList";
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "one of the most famous sky scraper in the world",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/12/08/00/40/empire-state-building-1081929_1280.jpg",
-    address: "20 W 34th St, New York, NY 10001, United States",
-    location: {
-      lng: -73.985648,
-      lat: 40.748612,
-    },
-    creator: "u1"
-  },
-  {
-    id: "p2",
-    title: "Stonehenge",
-    description: "one of the most famous prehistoric monument in the world",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2013/04/08/14/22/stonehenge-101801_1280.jpg",
-    address: "Salisbury SP4 7DE, United Kingdom",
-    location: {
-      lng: -1.826215,
-      lat: 51.179487,
-    },
-    creator: "u2"
-  }
-];
+import PlaceList from '../components/PlaceList';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const UserPlaces = () => {
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
-  return <PlaceList items={loadedPlaces} />;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`,
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (error) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </>
+  );
 };
 
 export default UserPlaces;
